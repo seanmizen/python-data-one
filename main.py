9bde770b02e813a7e11430bd3e390f87f64cc678
Sean <sean@seanmizen.com>
2021-09-19 21:10:57 +0100

    Imported more code (which is still a bit broke)

--- main.py
+++ main.py
@@ -11,16 +11,17 @@ import statsmodels.api as sm
 # for machine learning
 from sklearn import model_selection, preprocessing, feature_selection, ensemble, linear_model, metrics, decomposition
 # for explainer
-#from lime import lime_tabular
-
+# from lime import lime_tabular
 
 pandas_df = pd.read_csv("train.csv")
 dtf = pd.read_csv("train.csv")
 
-#pandas_df.set_index("Passenger ID")
+# pandas_df.set_index("Passenger ID")
 # plt.plot()
 # pandas_df.value_counts().plot().bar()
 
+'''
+# Section
 x = "Age"
 fig, ax = plt.subplots(nrows=1, ncols=2,  sharex=False, sharey=False)
 fig.suptitle(x, fontsize=20)
@@ -47,13 +48,13 @@ tmp_dtf = pd.DataFrame(dtf[x])
 tmp_dtf[x] = np.log(tmp_dtf[x])
 tmp_dtf.boxplot(column=x, ax=ax[1])
 plt.show()
+'''
 
 '''
+# Section
 for col in pandas_df.columns:
     print(col)
     print(pandas_df[col].nunique())
-
-
 
 Recognize whether a column is numerical or categorical.
 :parameter
@@ -68,7 +69,8 @@ def utils_recognize_type(dtf, col, max_cat=20):
         return "cat"
     else:
         return "num"
-dic_cols = {col:utils_recognize_type(dtf, col, max_cat=20) for col in dtf.columns}
+dic_cols = {col:utils_recognize_type(
+    dtf, col, max_cat=20) for col in dtf.columns}
 heatmap = dtf.isnull()
 for k,v in dic_cols.items():
  if v == "num":
@@ -77,5 +79,46 @@ for k,v in dic_cols.items():
    heatmap[k] = heatmap[k].apply(lambda x: 0 if x is False else 1)
 sns.heatmap(heatmap, cbar=False).set_title('Dataset Overview')
 plt.show()
-print("\033[1;37;40m Categerocial ", "\033[1;30;41m Numeric ", "\033[1;30;47m NaN ")
+print("\033[1;37;40m Categerocial ",
+      "\033[1;30;41m Numeric ", "\033[1;30;47m NaN ")
 '''
+
+
+# Section
+x = "Density"
+y = "Age"
+fig, ax = plt.subplots(nrows=1, ncols=3,  sharex=False, sharey=False)
+fig.suptitle(x + "   vs   " + y, fontsize=20)
+
+# distribution
+ax[0].title.set_text("density")
+sns.distplot(dtf[dtf["Survived"] == 1]["Age"], hist=False, label=1, ax=ax[0])
+sns.distplot(dtf[dtf["Survived"] == 0]["Age"], hist=False, label=0, ax=ax[0])
+ax[0].grid(True)
+
+# stacked
+ax[1].title.set_text("bins")
+breaks = np.quantile(dtf["Age"], q=np.linspace(0, 1, 11))
+tmp = dtf.groupby(
+    ["Survived", pd.cut(dtf["Age"], breaks, duplicates='drop')]).size().unstack().T
+tmp = tmp[dtf["Survived"].unique()]
+tmp["tot"] = tmp.sum(axis=1)
+for col in tmp.drop("tot", axis=1).columns:
+    tmp[col] = tmp[col] / tmp["tot"]
+tmp.drop("tot", axis=1).plot(kind='bar', stacked=True,
+                             ax=ax[1], legend=False, grid=True)
+
+# boxplot
+ax[2].title.set_text('outliers')
+sns.catplot(x="Survived", y="Age", data=dtf, kind="box", ax=ax[2])
+ax[2].grid(True)
+plt.show()
+
+# Section
+cat, num = "Y", "Age"
+model = smf.ols(num + " ~ " + cat, data=dtf).fit()
+table = sm.stats.anova_lm(model)
+p = table["PR(>F)"][0]
+coeff, p = None, round(p, 3)
+conclusion = "Correlated" if p < 0.05 else "Non-Correlated"
+print("Anova F: the variables are", conclusion, "(p-value: " + str(p) + ")")
